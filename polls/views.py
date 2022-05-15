bec9857f92941a6808a7b7cc5639c8a48606b640
AdminTima <tima.khilkevich@inbox.ru>
2022-05-15 14:36:13 +0300

    Function based views was replaced with class based views

--- polls/views.py
+++ polls/views.py
@@ -3,23 +3,41 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponse
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'polls/index.html', {'latest_questions': latest_questions})
+# def index(request):
+#     latest_questions = Question.objects.order_by('-pub_date')[:5]
+#     return render(request, 'polls/index.html', {'latest_questions': latest_questions})
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+#
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
