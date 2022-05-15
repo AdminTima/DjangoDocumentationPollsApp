5cebbfbfdfdb326a96e263a7027330f96cba99d6
AdminTima <tima.khilkevich@inbox.ru>
2022-05-15 14:11:19 +0300

    Added view to vote, check results, to display all questions

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponse
+from django.urls import reverse
+
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse('It is main page')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_questions': latest_questions})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    print(request.POST['choice'])
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',
+                      {'question': question, 'error_msg': 'You did not select any choice'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
